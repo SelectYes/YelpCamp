121cc426164ddbdbb5dedc07e846a2a64262daa2
Lenny <riel_lenny@yahoo.co.uk>
2020-07-24 15:14:51 -0500

    Added a new feature to add new camp grounds to the database

--- app.js
+++ app.js
@@ -4,25 +4,37 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+let campgrounds = [
+    {
+        name: "Salmon Creek", image:"https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"
+    },
+    {
+        name: "Yellow Valley", image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
+    },
+    {
+        name: "Witch Water Canyon", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    }
+];
 
 app.get('/', (req, res) => {
     res.render('landing');
 })
 
 app.get('/campgrounds', (req, res) => {
-    const campgrounds = [
-        {
-            name: "Salmon Creek", image:"https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"
-        },
-        {
-            name: "Yellow Valley", image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
-        },
-        {
-            name: "Witch Water Canyon", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-        }
-    ];
-
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', (req, res) => {
+    const name = req.body.name;
+    const image = req.body.image;
+    campgrounds.push({name: name, image: image});
+    res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new');
 })
 
 app.listen(port, console.log(`YelpCamp server has started on Localhost:${port}`));
