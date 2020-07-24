2f9f93716f9fedc21bb40fed4ffaefd84d4becd7
Lenny <riel_lenny@yahoo.co.uk>
2020-07-24 12:21:10 -0500

    First Commit.

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('landing');
+})
+
+app.get('/campgrounds', (req, res) => {
+    const campgrounds = [
+        {
+            name: "Salmon Creek", image:"https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"
+        },
+        {
+            name: "Yellow Valley", image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
+        },
+        {
+            name: "Witch Water Canyon", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        }
+    ];
+
+    res.render('campgrounds', {campgrounds: campgrounds});
+})
+
+app.listen(port, console.log(`YelpCamp server has started on Localhost:${port}`));
