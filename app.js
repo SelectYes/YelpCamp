7354a63bcef40995772d527025b54971582ce404
Lenny <riel_lenny@yahoo.co.uk>
2020-08-04 12:36:15 -0500

    Added comment model/section on show-page

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-// RESTFUL ROUTES
-// --------------
-
-// name    url         verb    description 
-// =======================================
-// INDEX   /dogs       GET     Display a list of all dogs 
-// NEW     /dogs/new   GET     Displays form to make a new dog 
-// CREATE  /dogs       POST    Add new dog to DB 
-// SHOW    /dogs/:id   GET     Shows info about one dog
-
-
-// {"name" : "Salmon Creek", "image" : "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"}
-// {"name" : "Yellow Valley", "image" : "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"}
-// {"name" : "Witch Water Canyon", "image" : "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
-// {"name" : "Greenpath", "image" : "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
-// {"name" : "Lonely Hill", "image" : "https://images.unsplash.com/photo-1515279831895-4a03671e5edf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=921&q=80"}
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                              CONFIG                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const express = require('express');
 const app = express();
@@ -22,10 +8,9 @@ const mongoose = require('mongoose');
 const { urlencoded } = require('body-parser');
 const { response } = require('express');
 const Campground = require('./models/campground');
-const seedDB = require('./seeds')
 const port = 3000;
+const seedDB = require('./seeds')
 
-seedDB();
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {
     useNewUrlParser: true,
@@ -38,20 +23,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                              ROUTES                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Campground.create({
-//     name: "Witch Water Canyon", 
-//     image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-//     description: "Enjoy a quiet weekend away from the city in this peaceful desert oasis. All the usual camping facilities - clean water, electricity and bathrooms."
-// }, (err, campground) => {
-//     if (err) {
-//         console.log('ERROR!');
-//         console.log(err);
-//     } else {
-//         console.log('NEW CAMPGROUND CREATED:');
-//         console.log(campground);
-//     }
-// })
+// seedDB();
 
 app.get('/', (req, res) => {
     res.render('landing');
@@ -96,7 +72,7 @@ app.get('/campgrounds/new', (req, res) => {
 
 // SHOW ROUTE (DISPLAYS INFO ABOUT ONE ENTRY)
 app.get('/campgrounds/:id', (req, res) => {
-    Campground.findById(req.params.id, (err, retrievedData) => {
+    Campground.findById(req.params.id).populate('comments').exec((err, retrievedData) => {
         if (err) {
             console.log(err);
         } else {
--- seeds.js
+++ seeds.js
@@ -1,14 +1,55 @@
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const Comment = require('./models/comment');
 
-const seedDB = () => {
-    Campground.deleteMany({}, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('DB cleared!');
-        }
-    });
+// SEEDS FOR FUTURE USE:
+
+// {"name" : "Salmon Creek", "image" : "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"}
+// {"name" : "Yellow Valley", "image" : "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"}
+// {"name" : "Witch Water Canyon", "image" : "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
+// {"name" : "Greenpath", "image" : "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
+// {"name" : "Lonely Hill", "image" : "https://images.unsplash.com/photo-1515279831895-4a03671e5edf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=921&q=80"}
+
+let seeds = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+];
+
+const seedDB = async () => {
+    try {
+        // REMOVE ALL CAMPGROUNDS
+        await Campground.deleteMany({}, () => console.log('CAMPGROUNDS REMOVED'));
+        await Comment.deleteMany({}, () => console.log('COMMENTS REMOVED'));
+    
+        // ADD A FEW CAMPGROUNDS
+        seeds.forEach(async seed => {
+            let campground = await Campground.create(seed);
+            let comment = await Comment.create({
+                text: "This campground is great. I just wish we had wifi.",
+                author: "Homer"
+            });
+            campground.comments.push(comment);
+            campground.save();
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+    // ADD A FEW COMMENTS
+
 }
 
 module.exports = seedDB;
