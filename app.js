cbb204145df22521c857c7cf4df29a82c0deb471
Lenny <riel_lenny@yahoo.co.uk>
2020-08-21 12:03:46 -0500

    added correct port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const methodOverride        = require('method-override');
 const User                  = require('./models/user');
 const Campground            = require('./models/campground');
 const Comment               = require('./models/comment');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 // routes
